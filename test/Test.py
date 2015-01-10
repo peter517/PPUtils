eb9817658e16064ad7c985acba777529a7becefc
pengjun <pengjun@pengjundeMacBook-Pro.local>
2015-01-10 14:44:43 +0800

    for personal mac test

--- test/Test.py
+++ test/Test.py
@@ -35,5 +35,31 @@ def hello():
     """a decorated hello world"""
     return "hello world"
 
+class Test:
+    def print1(self):
+        print "hello"
+    def print2(self):
+        print "123"
+        self.print1()
+        
 if __name__ == '__main__':
     print('result:{}   name:{}   doc:{}'.format(hello(), hello.__name__, hello.__doc__))
+    t =  Test();
+    t.print2();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
