a07834672a15fadacc9e27510ff39006ad2fc387
pengjun.pj <pengjun.pj@alibaba-inc.com>
2017-10-25 15:13:41 +0800

    refactor code

--- net/baiduimage.py
+++ net/baiduimage.py
@@ -13,7 +13,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # 设置超时
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(20)
 
 # 获取图片url内容等
 def getImagesURL(max_number, word='美女'):
@@ -21,7 +21,7 @@ def getImagesURL(max_number, word='美女'):
     pn = 0
     while pn <= max_number:
         url = 'http://image.baidu.com/search/avatarjson?tn=resultjsonavatarnew&ie=utf-8&word=' + search + '&cg=girl&pn=' + str(
-            pn) + '&rn=60&itg=0&z=0&fr=&width=&height=&lm=-1&ic=0&s=0&st=-1&gsm=1e0000001e'
+                pn) + '&rn=60&itg=0&z=0&fr=&width=&height=&lm=-1&ic=0&s=0&st=-1&gsm=1e0000001e'
         page = urllib.urlopen(url)
         data = page.read()
         data = json.loads(data)
@@ -37,47 +37,37 @@ def saveImage(json, word, max_number):
     # 判断名字是否重复
     index = len(os.listdir(wordDirPath))
     imagePath = wordDirPath + "/" + str(counter) + ".jpeg";
-    if os.path.exists(wordDirPath + "/" + str(counter) + ".jpeg"):
+
+    if os.path.exists(imagePath):
         counter = index
-    check = '<html><body><h1>403 Forbidden</h1>'
-    print "name " + word + " imagePath " + imagePath
 
     for info in json['imgs']:
-        urllib.urlretrieve(info['objURL'], './' + word + '/' + str(counter) + '.jpeg')
+        imagePath = wordDirPath + "/" + str(counter) + ".jpeg";
+        urllib.urlretrieve(info['objURL'], imagePath)
         # 判断防爬虫 <html><body><h1>403 Forbidden</h1>
-        fp = open("./" + word.replace("?","") + "/" + str(counter) + ".jpeg")
+        fp = open(wordDirPath + "/" + str(counter) + ".jpeg")
         if not cmp(fp.readline(), '<html><body><h1>403 Forbidden</h1>\n'):
-            os.remove("./" + word + "/" + str(counter) + ".jpeg")
+            os.remove(imagePath)
             counter -= 1
         fp.close()
         counter += 1
-
-        print "+1，已经保存" + str(counter - 1)
+        print "+1 save image total " + str(counter - 1)
         if counter > max_number:
             return
+
     return counter
 
-# 获取后缀名
-def getFix(name):
-    return name[name.find('.'):]
 
-# 获取前缀
-def getPrefix(name):
-    return name[:name.find('.')]
-
-# print getFix('123.txt')
-# getImagesURL(390,'美女')
-file_object1 = open("../test/diff_not_include.txt", 'r')
-
+# Main
 try:
-    srcList = file_object1.readlines()
+    words_file = open("../test/diff_not_include.txt", 'r')
+    word_lines = words_file.readlines()
+    for word in word_lines:
+        print "getImagesURL word " + word
+        try:
+            getImagesURL(10, word)
+        except Exception as e:
+            print e
 finally:
-    file_object1.close()
-
-for src in srcList:
-    print "getImagesURL " + src
-    try:
-        getImagesURL(10, src)
-    except Exception as e :
-        print e
+    words_file.close()
 
